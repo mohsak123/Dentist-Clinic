2a9c979338f6bd3821798492f87c03c543275b5c
mohammed.sh <mohammedshakokah@gmail.com>
2025-09-06 14:08:53 +0300

    push

---
+++ src/components/bars/NavbarDoctor.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useAuth } from '@/context/AuthContext';
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation';
+import React from 'react'
+
+const NavbarDoctor = () => {
+
+    const { isAuthenticated, logout } = useAuth();
+    const router = useRouter();
+
+    const handleLogout = () => {
+      logout();
+      router.push('/');
+    };
+
+  return (
+    <div>
+      <div className='flex items-center justify-between px-4 sm:px-10 py-3 border-b bg-white'>
+        <Link href="/" className='flex items-center gap-2 text-xs'>
+          <Image src="/images/logo.svg" alt='logo' width={217} height={46}
+            className='w-36 md:w-40 cursor-pointer'
+          />
+          <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>Doctor</p>
+        </Link>
+        <button onClick={handleLogout} className='bg-mainColor text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+      </div>
+    </div>
+  )
+}
+
+export default NavbarDoctor
